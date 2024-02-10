e7c43a6a2cef811cfd9a70715fba2d500728b4b4
satheeshkumar <satheeshkumarnsn@gmail.com>
2024-02-10 18:36:48 +0530

    Project Setup

---
+++ app/src/main/java/com/diamondpick/myapplication/newsreader/NewsReaderActivity.java
@@ -0,0 +1,16 @@
+package com.diamondpick.myapplication.newsreader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.diamondpick.myapplication.R;
+
+public class NewsReaderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_reader);
+    }
+}
